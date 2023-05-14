d1d7186442bdd0412534d56a3c00152faa7d5016
TorresKwok <fanztorres@gmail.com>
2023-05-14 14:25:07 -0700

    Progress bar start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from "./components/navBar"
 import USMap from "./components/USMap"
+import ProgressBar from "./components/progressBar"
 
 import "./App.css"
 
@@ -8,6 +9,7 @@ function App() {
 		<>
 			<NavBar />
 			<USMap />
+			<ProgressBar />
 		</>
 	)
 }
